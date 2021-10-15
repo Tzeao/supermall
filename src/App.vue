cd1c47d0f7aa689252fe0dda4d9c9c21a43eb081
Tzeao <773238750@qq.com>
2021-10-15 20:42:18 +0800

    底部导航栏与页面组件进行路由绑定

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
     <main-tab-bar/>
+    <router-view></router-view>
   </div>
 </template>
 <script>
 import MainTabBar from 'components/content/mainTabBar/MainTabBar'
+
 export default {
   name:'APP',
   components: {
---
+++ src/router/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+const Home = () =>
+    import ('views/home/Home.vue')
+
+const Category = () =>
+    import ('views/category/Category.vue')
+
+const Cart = () =>
+    import ('views/cart/Cart.vue')
+
+const Profile = () =>
+    import ('views/profile/Profile.vue')
+
+Vue.use(VueRouter)
+    // 导航重复
+    //获取原型对象上的replace函数
+const originalReplace = VueRouter.prototype.replace
+    //修改原型对象中的replace方法
+VueRouter.prototype.replace = function replace(location) {
+    return originalReplace.call(this, location).catch(err => err)
+}
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/category',
+        name: 'Category',
+        component: Category
+    },
+    {
+        path: '/cart',
+        name: 'Cart',
+        component: Cart
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: Profile
+    }
+]
+
+const router = new VueRouter({
+    mode: 'hash',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
