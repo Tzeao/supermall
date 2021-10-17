c977abed30a9c0ec20d80c91881f1d9ed91e0351
Tzeao <773238750@qq.com>
2021-10-17 14:43:51 +0800

    封装一个可选的导航栏

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tab-control">
+    <div v-for="item in titles">
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    tutles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+   
+<style>
+</style>
