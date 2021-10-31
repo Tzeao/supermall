ff7e3d9e7da68ba584ab47bd8719f977837843ff
Tzeao <773238750@qq.com>
2021-10-31 21:16:29 +0800

    使用fastclick减少点击延迟

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import fastclick from 'fastclick'
+
 import toast from 'common/toast/index'
 
 Vue.config.productionTip = false
@@ -12,6 +14,8 @@ Vue.prototype.$bus = new Vue()
 // 安装Toast插件
 Vue.use(toast)
 
+// 解决移动端的300ms 延迟
+fastclick.attach(document.body)
 new Vue({
     render: h => h(App),
     router,
