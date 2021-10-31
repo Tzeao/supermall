e3e9414f3712b3a5d5ca400b7e629678811a4b39
Tzeao <773238750@qq.com>
2021-10-31 14:49:34 +0800

    购物车商品列表展示和选择按钮封装

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,21 +1,27 @@
 <template>
-  <div>
+  <div class="car">
     <nav-bar class="nav">
       <div slot="center">购物车({{cartLength}})</div>
     </nav-bar>
+
+    <cart-list/>
   </div>
 </template>
 
 <script>
 import NavBar from "common/navbar/NavBar";
 
+import CartList from './childComps/CartList'
+
 // 可以把函数直接转为计算属性
 import {mapGetters} from 'vuex'
+
 
 export default {
   name: "Cart",
   components: {
     NavBar,
+    CartList
   },
   computed:{
     // 语法 写成数组表示一一对应 写成对象表示名字不一样
@@ -25,6 +31,9 @@ export default {
 </script >
 
 <style scoped>
+.car{
+  height: 100vh;
+}
 .nav{
  background-color: var(--color-tint); 
  color: wheat;
---
+++ src/views/cart/childComps/CartList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="cart-list">
+    <scroll class="content" ref="scroll">
+      <cart-list-item
+        v-for="(item, index) in cartList"
+        :itemInfo="item"
+        :key="index"
+      />
+    </scroll>
+  </div>
+</template>
+
+<script>
+import Scroll from "common/scroll/Scroll";
+
+import CartListItem from "./CartListItem";
+
+import { mapGetters } from "vuex";
+export default {
+  name: "CartList",
+  components: {
+    Scroll,
+    CartListItem,
+  },
+  computed: {
+    ...mapGetters(["cartList"]),
+  },
+  // 因为添加完数据scroll不知道我们添加数据了，所以要人为重新计算
+  activated() {
+    this.$refs.scroll.refresh();
+  },
+};
+</script>
+
+<style scoped>
+.cart-list {
+  height: calc(100% - 44px - 49px);
+}
+.content {
+  height: 100%;
+  overflow: hidden;
+}
+</style>
